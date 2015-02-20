d7e84b156dcc517a6bc3b1750a68f6b85f2929d8
hristotrifonov <11060905@studentmail.ul.ie>
2015-02-20 10:55:35 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,9 @@
-/*
-
-	Module  : Security _ Communication Protocols
-	Code    : EE6032
-	Created : 09/02/2015
-
-	group members : 
-					member1    00000000
-					member2    00000000
-					member3    00000000
-
-*/
-
-
 // LinkSample.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
 
 #include <Windows.h>	// needed by CHILKAT e.g. for SystemTime
-
 // added for CHILKAT
 #include "..\include\ckimap.h" 
 //#include "..\include\tchar.h"
@@ -46,28 +31,6 @@ void RSA_ex(void);
 
 //Function to pause your program - for debugging purposes. Could also use this command [ system("pause"); ]
 void key_press(void);
-
-// This function is just to see if your files compile and link ok........its not needed for the project in EE6032/ED5012
-void DoNothing(void);
-
-void DoNothing(void)
-{
-	CkImap imap;
-	imap.UnlockComponent("T12302015IMAPMAILQ_gdXOQ92bIRDN");
-	imap.Connect("imap.gmail.com");
-
-	CkFtp2W ftp2W;
-
-	bool success = ftp2W.UnlockComponent(L"T12302015FTP_yJTfpTRHIR5l");
-
-    HCkSFtpW c_sftp = CkSFtpW_Create();
-    int isuccess = CkSFtpW_UnlockComponent(c_sftp,L"T12302015FTP_yJTfpTRHIR5l");
-    CkSFtpW_Dispose(c_sftp);
-
-    HCkImap c_imap = CkImap_Create();
-    isuccess = CkImap_UnlockComponent(c_imap,"T12302015IMAPMAILQ_gdXOQ92bIRDN");
-    CkImap_Dispose(c_imap);
-}
 
 void key_press(void)
 {
@@ -108,6 +71,8 @@ void RSA_ex(void)
     publicKey = rsa.exportPublicKey();
     const char * privateKey;
     privateKey = rsa.exportPrivateKey();
+
+	// Store the 2 keys at this point for later use.
 
     const char * plainText;
     plainText = "Encrypting and decrypting should be easy!";
@@ -233,73 +198,10 @@ void SHA1_ex(void)
 	printf("SHA1:\n");
 	printf("%s\n", hash);
 
-	//  Hash using MD2
-	crypt.put_HashAlgorithm("md2");
-	hash = crypt.hashStringENC(s);
-	printf("MD2:\n");
-	printf("%s\n", hash);
-
-	//  Hash using MD5
-	crypt.put_HashAlgorithm("md5");
-	hash = crypt.hashStringENC(s);
-	printf("MD5:\n");
-	printf("%s\n", hash);
-
-	//  Note: SHA-2 is a set of cryptographic hash functions (SHA-224, SHA-256, SHA-384, SHA-512)
-
-	//  Hash using SHA-256
-	crypt.put_HashAlgorithm("sha256");
-	hash = crypt.hashStringENC(s);
-	printf("SHA256:\n");
-	printf("%s\n", hash);
-
-	//  Hash using SHA-384
-	crypt.put_HashAlgorithm("sha384");
-	hash = crypt.hashStringENC(s);
-	printf("SHA384:\n");
-	printf("%s\n", hash);
-
-	//  Hash using SHA-512
-	crypt.put_HashAlgorithm("sha512");
-	hash = crypt.hashStringENC(s);
-	printf("SHA512:\n");
-	printf("%s\n", hash);
-
-	//  Hash using HAVAL
-	//  There are two additional properties relevant to HAVAL:
-	//  HavalRounds, and KeyLength.
-	//  HavalRounds can have values of 3, 4, or 5.
-	//  KeyLength can have values of 128, 160, 192, 224, or 256
-	crypt.put_HashAlgorithm("haval");
-	crypt.put_HavalRounds(5);
-	crypt.put_KeyLength(256);
-	hash = crypt.hashStringENC(s);
-	printf("Haval:\n");
-	printf("%s\n", hash);
-
 	//  Hashes for "The quick brown fox jumps over the lazy dog"
 
 	//  SHA1:
 	//  2FD4E1C67A2D28FCED849EE1BB76E7391B93EB12
-
-	//  MD2:
-	//  03D85A0D629D2C442E987525319FC471
-
-	//  MD5:
-	//  9E107D9D372BB6826BD81D3542A419D6
-
-	//  SHA256:
-	//  D7A8FBB307D7809469CA9ABCB0082E4F8D5651E46D3CDB762D02D0BF37C9E592
-
-	//  SHA384:
-	//  CA737F1014A48F4C0B6DD43CB177B0AFD9E5169367544C494011E3317DBF9A509CB1E5DC1E85A941BBEE3D7F2AFBC9B1
-
-	//  SHA512:
-	//  07E547D9586F6A73F73FBAC0435ED76951218FB7D0C8D788A309D785436BBB642E93A252A954F23912547D1E8A3B5ED6E1BFD7097821233FA0538F3DB854FEE6
-
-	//  Haval:
-	//  B89C551CDFE2E06DBD4CEA2BE1BC7D557416C58EBB4D07CBC94E49F710C55BE4
-
 }
 
 
@@ -307,10 +209,11 @@ void SHA1_ex(void)
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	DoNothing();
-	SHA1_ex();
+
 	RSA_ex();
 	AES_ex();
+	SHA1_ex();
 	key_press();
 	return 0;
 }
+
